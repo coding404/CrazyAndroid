13d0a618717b6abb9512c8436f27f7312d625e72
liushu <liushu@mysteelsoft.com.cn>
2017-02-07 16:20:20 +0800

    添加忽略

--- app/src/main/java/com/dg/user008/crazyandroid/fragment/Chapter02Fragment.java
+++ app/src/main/java/com/dg/user008/crazyandroid/fragment/Chapter02Fragment.java
@@ -10,8 +10,14 @@ import android.view.ViewGroup;
 import android.widget.ExpandableListView;
 
 import com.dg.user008.crazyandroid.R;
+import com.dg.user008.crazyandroid.activity.Demo020101Activity;
 import com.dg.user008.crazyandroid.activity.Demo020102Activity;
-import com.dg.user008.crazyandroid.activity.DemoOne020101Activity;
+import com.dg.user008.crazyandroid.activity.Demo020103Activity;
+import com.dg.user008.crazyandroid.activity.Demo020201Activity;
+import com.dg.user008.crazyandroid.activity.Demo020202Activity;
+import com.dg.user008.crazyandroid.activity.Demo020203Activity;
+import com.dg.user008.crazyandroid.activity.Demo020204Activity;
+import com.dg.user008.crazyandroid.activity.Demo020205Activity;
 import com.dg.user008.crazyandroid.activity.FileUtil;
 import com.dg.user008.crazyandroid.adapter.MyExpandableListViewAdapter;
 import com.dg.user008.crazyandroid.bean.CatalogBean;
@@ -48,8 +54,8 @@ public class Chapter02Fragment extends Fragment {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
                 //界面跳转
-                JumpActivity(groupPosition,childPosition);
-              //  Toast.makeText(getContext(),"groupPosition="+groupPosition+",childPosition="+childPosition,Toast.LENGTH_SHORT).show();
+                JumpActivity(groupPosition, childPosition);
+                //  Toast.makeText(getContext(),"groupPosition="+groupPosition+",childPosition="+childPosition,Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
@@ -57,194 +63,209 @@ public class Chapter02Fragment extends Fragment {
 
     private void JumpActivity(int groupPosition, int childPosition) {
 
-        switch (groupPosition){
+        switch (groupPosition) {
             case 0:
-                if (childPosition==0){
-                    Intent intent0000=new Intent(getContext(), DemoOne020101Activity.class);
+                if (childPosition == 0) {
+                    //用编程的方式开发UI界面
+                    Intent intent0000 = new Intent(getContext(), Demo020101Activity.class);
                     startActivity(intent0000);
 
-                }else if (childPosition==1){
-                    Intent intent0001 =new Intent(getContext(), Demo020102Activity.class);
+                } else if (childPosition == 1) {
+                    //简单图片浏览器
+                    Intent intent0001 = new Intent(getContext(), Demo020102Activity.class);
                     startActivity(intent0001);
 
-                }else if(childPosition==2){
-
+                } else if (childPosition == 2) {
+                    //跟随手指的小球
+                    Intent intent0002 = new Intent(getContext(), Demo020103Activity.class);
+                    startActivity(intent0002);
 
                 }
                 break;
             case 1:
-                if (childPosition==0){
+                if (childPosition == 0) {
+                    //丰富的表格布局
+                    Intent intent0100 = new Intent(getContext(), Demo020201Activity.class);
+                    startActivity(intent0100);
+
+                } else if (childPosition == 1) {
+                    //霓虹灯效果
+                    Intent intent0101 = new Intent(getContext(), Demo020202Activity.class);
+                    startActivity(intent0101);
+
+                } else if (childPosition == 2) {
+                    //梅花布局效果
+                    Intent intent0102 = new Intent(getContext(), Demo020203Activity.class);
+                    startActivity(intent0102);
+
+                } else if (childPosition == 3) {
+                    //计算器界面
+                    Intent intent0103 =new Intent(getContext(), Demo020204Activity.class);
+                    startActivity(intent0103);
 
 
-                }else if (childPosition==1){
-
-
-                }else if(childPosition==2){
-
-
-                }else if (childPosition==3){
-
-
-                }else if (childPosition==4){
-
+                } else if (childPosition == 4) {
+                    //登录界面
+                    Intent intent0104 =new Intent(getContext(), Demo020205Activity.class);
+                    startActivity(intent0104);
 
                 }
                 break;
             case 2:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
-                }else if (childPosition==3) {
+                } else if (childPosition == 3) {
 
 
-                }else if (childPosition==4){
+                } else if (childPosition == 4) {
 
 
-                }else if (childPosition==5){
+                } else if (childPosition == 5) {
 
 
-                }else if (childPosition==6){
+                } else if (childPosition == 6) {
 
 
                 }
                 break;
             case 3:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
                 }
                 break;
             case 4:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
-                }else if (childPosition==3) {
+                } else if (childPosition == 3) {
 
 
-                }else if (childPosition==4){
+                } else if (childPosition == 4) {
 
 
-                }else if (childPosition==5){
+                } else if (childPosition == 5) {
 
 
-                }else if (childPosition==6){
+                } else if (childPosition == 6) {
 
 
-                }else if (childPosition==7){
+                } else if (childPosition == 7) {
 
 
-                }else if (childPosition==8){
+                } else if (childPosition == 8) {
 
 
                 }
                 break;
             case 5:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
                 }
                 break;
             case 6:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
                 }
                 break;
             case 7:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
-                }else if (childPosition==3) {
+                } else if (childPosition == 3) {
 
 
-                }else if (childPosition==4){
+                } else if (childPosition == 4) {
 
 
-                }else if (childPosition==5){
+                } else if (childPosition == 5) {
 
 
-                }else if (childPosition==6){
+                } else if (childPosition == 6) {
 
 
-                }else if (childPosition==7){
+                } else if (childPosition == 7) {
 
 
                 }
                 break;
             case 8:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
-                }else if (childPosition==3) {
+                } else if (childPosition == 3) {
 
 
-                }else if (childPosition==4){
+                } else if (childPosition == 4) {
 
 
-                }else if (childPosition==5){
+                } else if (childPosition == 5) {
 
 
                 }
                 break;
             case 9:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
                 }
                 break;
             case 10:
-                if (childPosition==0){
+                if (childPosition == 0) {
 
 
-                }else if (childPosition==1){
+                } else if (childPosition == 1) {
 
 
-                }else if(childPosition==2){
+                } else if (childPosition == 2) {
 
 
-                }else if (childPosition==3) {
+                } else if (childPosition == 3) {
 
 
                 }
@@ -254,14 +275,12 @@ public class Chapter02Fragment extends Fragment {
         }
 
 
-
-
     }
 
     private void initData() {
         mBeen = new ArrayList<>();
         String s = FileUtil.readFromAssets(getContext(), "chapter02.txt");
-        Gson gson=new Gson();
+        Gson gson = new Gson();
         CatalogBean catalogBean = gson.fromJson(s, CatalogBean.class);
         mBeen.clear();
         mBeen.addAll(catalogBean.getChapters());
