73f45f32ceee23b6c7e0699151ef7c25d825df8d
liushu <liushu@mysteelsoft.com.cn>
2017-02-07 16:21:18 +0800

    添加忽略

---
+++ app/src/main/java/com/dg/user008/crazyandroid/activity/Demo020101Activity.java
@@ -0,0 +1,38 @@
+package com.dg.user008.crazyandroid.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Date;
+
+/**
+ * Created by liushu on 2017/2/6.
+ */
+
+public class Demo020101Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout layout = new LinearLayout(this);
+        super.setContentView(layout);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        final TextView textView = new TextView(this);
+        Button button = new Button(this);
+        button.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        button.setText("ok");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello Android!"+new Date());
+            }
+        });
+        layout.addView(textView);
+        layout.addView(button);
+
+    }
+}
